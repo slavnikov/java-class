90f1cacb5a4da20d024aac85b42adc8ca3b1010b
Dmitry Slavnikov <dmitry.slavnikov@gmail.com>
2018-11-03 18:02:08 -0400

    Practice creating variables.

--- hello_world/HelloWorld.java
+++ hello_world/HelloWorld.java
@@ -1,5 +1,9 @@
 public class HelloWorld {
    public static void main(String[] args) {
      System.out.println("Hello World! I am a Java program.");
+
+     int myFirstNumber = 5 + 2;
+     System.out.println(myFirstNumber);
+     System.out.println("mySecondNumber");
    }
 }
