973fc803fd4ccfb731b122e553b04caf184bb9c8
Dmitry Slavnikov <dmitry.slavnikov@gmail.com>
2018-11-03 19:29:22 -0400

    Practice with floats and doubles.

---
+++ floats/Floats.java
@@ -0,0 +1,19 @@
+public class Floats {
+   public static void main(String[] args) {
+     int intValue = 5; // => 5
+     int thirdInt = 5 / 3; // => 1
+     float floatValue = 5f; // => 5.0
+     float thirdFload = 5f / 3f;
+     double doubleValue = 5d; // => 5.0
+     double thirdDouble = 5d / 3d;
+
+     System.out.println(thirdInt);
+     System.out.println(thirdFload);
+     System.out.println(thirdDouble);
+
+     int pounds = 5;
+     double kilograms = pounds * 0.453;
+
+     System.out.println(kilograms);
+   }
+}
