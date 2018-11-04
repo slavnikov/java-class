33ee7492a7eb7d5e8e2d00f0ed4d0b8981000009
Dmitry Slavnikov <dmitry.slavnikov@gmail.com>
2018-11-04 10:16:57 -0500

    Create and use strings.

---
+++ strings/Strings.java
@@ -0,0 +1,8 @@
+public class Strings {
+  public static void main(String[] args) {
+    String myString = "This is my string.";
+    myString = myString + " " + myString;
+
+    System.out.println(myString);
+  }
+}
