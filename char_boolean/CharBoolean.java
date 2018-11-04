2a387cfb8152c419f7c47676938d4bf507fcc10b
Dmitry Slavnikov <dmitry.slavnikov@gmail.com>
2018-11-04 10:10:59 -0500

    Use char and boolean data types.

---
+++ char_boolean/CharBoolean.java
@@ -0,0 +1,13 @@
+public class CharBoolean {
+  public static void main(String[] args) {
+    char myChar = 'D';
+    char uniChar = '\u0053';
+
+    System.out.println(myChar);
+    System.out.println(uniChar);
+
+    boolean amAwesome = true;
+
+    System.out.println(amAwesome);
+  }
+}
